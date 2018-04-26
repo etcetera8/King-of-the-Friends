42617924f4ec750bf7abff3c8501d25d62627bed
parker lindley <fugazi8@gmail.com>
2018-04-25 19:26:49 -0600

    Add mocha chai server test setup

---
+++ routes.spec.js
@@ -0,0 +1,7 @@
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
