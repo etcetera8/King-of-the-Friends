53d834362abf120e3917677d544003a0d4a915f6
parker lindley <fugazi8@gmail.com>
2018-05-17 07:49:38 -0600

    Add endpoint for getting polyline coordinates

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+var polyline = require('@mapbox/polyline');
+var fetch = require("node-fetch");
+const axios = require('axios');
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
-
+const key = require('./apikey.js')
 app.set('port', process.env.PORT || 8001);
 
 app.locals.title = "kom db";
 app.use(bodyParser.json());
+
+app.post('/api/v1/route', async (request, response) => {
+  let { startLocation, destination } = request.body
+  axios.get(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLocation.latitude},${startLocation.longitude}&destination=${destination.latitude},${destination.longitude}&mode=walking&key=${key}`)
+    .then(result => result.data)
+    .then(result => {
+      let array = polyline.decode(result.routes[0].overview_polyline.points);
+      let coordinates = array.map((point) => {
+        return {
+          latitude: point[0],
+          longitude: point[1]
+        }
+      })
+      response.send(coordinates)
+    }).catch(error => console.log(error.message)) 
+})
 
 app.get('/api/v1/users', (request, response) => {
   database('users').select()
