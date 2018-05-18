f030ffc0bf0c9e0d667f67157b23eed57eb09ee3
parker lindley <fugazi8@gmail.com>
2018-05-18 15:53:53 -0600

    Login with strava success

--- api.js
+++ api.js
@@ -10,3 +10,8 @@ export const allApiCall = async (url) => {
   const parsedData = await response.json();
   return parsedData;
 }
+
+export const stravaLogin = async() => {
+  let response = await fetch(`https://www.strava.com/oauth/authorize?client_id=25688&response_type=code&redirect_uri=http://localhost:8001/exchange_token&approval_prompt=force`)
+  return response.url;
+}
--- components/Login.js
+++ components/Login.js
@@ -1,26 +1,46 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, Linking } from 'react-native';
 import { loginUser } from '../actions/index';
-import { apiCall } from '../api';
+import { apiCall, stravaLogin } from '../api';
+import Expo, { WebBrowser, AuthSession } from 'expo';
+
 class Login extends Component {
+  state = {
+    redirectData: null,
+  }
 
   loginHelper = async () => {
     const user = await apiCall('http://localhost:8001/api/v1/users/', 1);
     console.log(user)
-    await this.props.loginUser(user)
+    await this.props.loginUser(user) 
   }
 
-  render() {
+  render () {
+    console.log(WebBrowser);
     return(
       <View style={styles.container}>
         <Button
-          title="LOGIN"
+          title="UI QUICK LOGIN"
           onPress={() => this.loginHelper()}
         ></Button>
+        <Button
+          onPress={this._openAuthSessionAsync}
+          title="Login with Strava"
+        />
       </View>
     )
   }
+
+  _openAuthSessionAsync = async () => {
+    let redirect = await Linking.getInitialURL('/')
+    let result = await WebBrowser.openAuthSessionAsync(
+      `https://www.strava.com/oauth/authorize?client_id=25688&response_type=code&redirect_uri=${redirect}&approval_prompt=force`
+    );
+
+    this.setState({ result });
+  };
+
 }
 
 const mapStateToProps = (state) => ({
