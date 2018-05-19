ec29ce184efc5aab0ebc766f4988c6fab547234c
parker lindley <fugazi8@gmail.com>
2018-05-19 08:02:29 -0600

    Add loading gif on login

--- components/Login.js
+++ components/Login.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-import { StyleSheet, View, Text, Button, Linking } from 'react-native';
+import { StyleSheet, View, Text, Button, Linking, Image } from 'react-native';
 import Expo, { WebBrowser, AuthSession } from 'expo';
 import { loginUser, getTeam, getMembers } from '../actions/index';
 import { apiCall, allApiCall, stravaLogin, getUser } from '../api';
 import { cleanUser } from '../cleaner';
 
 class Login extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loading: false
+    }
+  } 
   // loginHelper = async () => {
   //   const user = await apiCall('http://localhost:8001/api/v1/users/', 1);
   //   console.log(user)
@@ -20,15 +26,21 @@ class Login extends Component {
           title="UI QUICK LOGIN"
           onPress={() => this.loginHelper()}
         ></Button> */}
+        { !this.state.loading ?
         <Button
           onPress={this._openAuthSessionAsync}
           title="Login with Strava"
         />
+        : <Image 
+            style={styles.loader}
+            source={require('../assets/loader.gif')}/>
+        }
       </View>
     )
   }
 
   _openAuthSessionAsync = async () => {
+    this.setState({loading: true})
     const redirect = await Linking.getInitialURL('/')
     const result = await WebBrowser.openAuthSessionAsync(
       `https://www.strava.com/oauth/authorize?client_id=25688&response_type=code&redirect_uri=${redirect}&approval_prompt=force`
@@ -54,6 +66,7 @@ class Login extends Component {
     const beUser = await apiCall('http://localhost:8001/api/v1/users/', user.email);
     const team = await apiCall(`http://localhost:8001/api/v1/team/`, beUser.team_id);
     const teamMembers = await allApiCall(`http://localhost:8001/api/v1/teamid?teamid=${beUser.team_id}`);
+    this.setState({ loading: false })
     this.props.getMembers(teamMembers)
     this.props.getTeam(team);
   }
@@ -78,6 +91,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignContent: 'center',
-    backgroundColor: "white",
+    backgroundColor: 'white',
+  },
+  loader: {
+    display: 'flex',
+    alignSelf: 'center',
   }
 })
