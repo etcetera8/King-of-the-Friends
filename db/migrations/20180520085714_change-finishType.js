fd6c05c357244cf5b5ae4e791d52a2ad1620382c
parker lindley <fugazi8@gmail.com>
2018-05-22 14:38:11 -0600

    Run migrations for start of challenge date for comparison of when to check

--- db/migrations/20180520085714_change-finishType.js
+++ db/migrations/20180520085714_change-finishType.js
@@ -1,14 +1,18 @@
 
 exports.up = function(knex, Promise) {
-  knex.schema.table('team', function (table) {
-    table.dropColumn('finish_date');
-    table.string('finish_date');
-  })
+  return Promise.all([
+    knex.schema.table('team', function (table) {
+      table.dropColumn('finish_date');
+      table.string('finish_date');
+    })
+  ])
 };
 
 exports.down = function(knex, Promise) {
-  knex.schema.table('team', function (table) {
-    table.dropColumn('finish_date');
-    table.dateTime('finish_date')
-  })
+  return Promise.all([
+    knex.schema.table('team', function (table) {
+      table.dropColumn('finish_date');
+      table.dateTime('finish_date')
+    })
+  ])
 };
---
+++ db/migrations/20180522103327_add-start-date.js
@@ -0,0 +1,15 @@
+exports.up = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.table('team', function (table) {
+      table.string('start_date');
+    })
+  ])
+};
+
+exports.down = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.table('team', function (table) {
+      table.dropColumn('start_date');
+    })
+  ])
+};
