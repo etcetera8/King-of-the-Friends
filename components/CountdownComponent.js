e2295e16ea64ff04fd09ba9dcc4b4c345e1c8993
parker lindley <fugazi8@gmail.com>
2018-05-28 14:50:07 -0600

    Connect to store, break out render from format function

--- components/CountdownComponent.js
+++ components/CountdownComponent.js
@@ -1,54 +1,60 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
 import RNMomentCountDown from 'react-native-moment-countdown';
 import CountDown from 'react-native-countdown-component';
 import moment from 'moment';
 
-export class CountdownComponent extends Component {
+class CountdownComponent extends Component {
   constructor(props) {
     super(props)
     this.state = {
       today: "",
       end: "",
-      seconds: 25
+      seconds: 0,
     }
   }
 
-componentDidMount() {
-  this.formatDate();
+componentDidMount = () => {
+  this.setState({seconds: this.formatDate(this.props.team.finish_date)})
 }
 
-formatDate = () => {
+formatDate = (date) => {
   if (this.props.date) {
     const today = new Date(Date.now()).toISOString();
     const now = moment(today);
-    const end = moment(this.props.date);
+    const end = moment(this.props.team.finish_date);
     const duration = moment.duration(now.diff(end));
     const seconds = Math.abs(duration._milliseconds)/1000;
-    return (<CountDown
-      digitBgColor={"rgba(242, 100, 48, 1)"}
-      digitTxtColor={"#fff"}
-      timeTxtColor={"#000"}
-      style={styles.countdown}
-      until={seconds}
-      onFinish={() => console.log('finished')}
-      onPress={() => alert('hello')}
-      size={20}
-    />)
+    return seconds
   }
 }
 
 render () {
-  let { date } = this.props;
+  const { seconds } = this.state;
     return (
       <View>
-        { date &&
-          this.formatDate()
+        { seconds &&
+          <CountDown
+            digitBgColor={"rgba(242, 100, 48, 1)"}
+            digitTxtColor={"#fff"}
+            timeTxtColor={"#000"}
+            style={styles.countdown}
+            until={seconds}
+            onFinish={() => console.log('finished')}
+            size={20}
+          />
         }
       </View>
     )
   }
 }
+
+const mapStateToProps = state => ({
+  team: state.team
+})
+
+export default connect(mapStateToProps, null)(CountdownComponent)
 
 const styles = StyleSheet.create({
   countdown: {
