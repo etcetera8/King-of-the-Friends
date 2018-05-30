6949af9b469d0a68e66406eccbf92eaf4daa1fb7
parker lindley <fugazi8@gmail.com>
2018-05-30 13:50:17 -0600

    Add default state for all props

--- actions/index.js
+++ actions/index.js
@@ -1,14 +1,14 @@
-export const loginUser = (user) => ({
+export const loginUser = (user = {}) => ({
   type: 'LOGIN_USER',
   payload: user
 });
 
-export const getTeam = (team) => ({
+export const getTeam = (team = {}) => ({
   type: 'GET_TEAM',
   payload: team
 })
 
-export const getMembers = (members) => ({
+export const getMembers = (members = []) => ({
   type: 'GET_MEMBERS',
   payload: members
 })
