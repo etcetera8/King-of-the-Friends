e28d2654547db0537d7ea8176b214bde7b25c1c6
parker lindley <fugazi8@gmail.com>
2018-06-04 11:09:30 -0600

    Instantly updating store and backend on user switching and or joining team

--- components/Account.js
+++ components/Account.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { StyleSheet, Text, View, Picker, Button } from 'react-native';
-import { apiCall, allApiCall, patchPostCall, segmentCall, serverRoot } from '../api';
+import { apiCall, allApiCall, patchPostCall, segmentCall, serverRoot, getUserAttempts } from '../api';
 import { TeamCreator } from './TeamCreator';
 import { loginUser, getTeam, getMembers, addCoordinates } from '../actions/index';
 import polyline from '@mapbox/polyline';
@@ -47,19 +47,48 @@ class Account extends Component {
           team_id: teamId
         })
       }
-      const confirmation = await patchPostCall(`${serverRoot}users/`, email, options);
-      const team = await apiCall(`${serverRoot}team/`, teamId);
-      const teamMembers = await allApiCall(`${serverRoot}teamid?teamid=${teamId}`);
-      const stravaSegment = await segmentCall(team.segment_id, token);
-      const coordinates = polyline.decode(stravaSegment.map.polyline).map(latLng => {
-         return { latitude: latLng[0], longitude: latLng[1] }
-      })
-
-      this.props.updateMembers(teamMembers)
-      this.props.updateTeam(team);
-      this.props.updateCoordinates(coordinates)
-      this.toggleAlert()
+      await this.updateStoreAndProps(email, token, teamId, options);
+      this.updateUserPropsAndBe()
     }
+  }
+
+  updateStoreAndProps = async (email, token, teamId, options) => {
+    const confirmation = await patchPostCall(`${serverRoot}users/`, email, options);
+    const team = await apiCall(`${serverRoot}team/`, teamId);
+    const teamMembers = await allApiCall(`${serverRoot}teamid?teamid=${teamId}`);
+    const stravaSegment = await segmentCall(team.segment_id, token);
+    const coordinates = polyline.decode(stravaSegment.map.polyline).map(latLng => {
+      return { latitude: latLng[0], longitude: latLng[1] }
+    })
+    this.props.updateMembers(teamMembers);
+    this.props.updateTeam(team);
+    this.props.updateCoordinates(coordinates);
+    this.toggleAlert();
+  }
+
+  updateUserPropsAndBe = async () => {
+    //need to get the newly chosen segment
+    const { token, email } = this.props.user;
+    const { start_date, segment_id } = this.props.team;
+    const attempts = await getUserAttempts(segment_id, token);
+    const attemptsToDate = attempts.filter(attempt => attempt.start_date > start_date);
+    const fastestTime = attemptsToDate.length ? attemptsToDate[0].elapsed_time : 0;
+    const updatedTime = { segment_time: fastestTime };
+    const updatedMembers = this.props.members.map( member =>  member.email === email ? {...member, ...updatedTime} : member );
+    this.props.updateMembers(updatedMembers)
+
+    const options = {
+      method: 'PATCH',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        segment_time: fastestTime,
+        token
+      })
+    }
+    const result = await patchPostCall('http://localhost:8001/api/v1/users/', email, options)
   }
 
   render() {
@@ -120,7 +149,9 @@ const styles = StyleSheet.create({
 });
 
 const mapStateToProps = state => ({
-  user: state.user
+  user: state.user,
+  team: state.team,
+  members: state.members
 })
 
 const mapDispatchToProps = dispatch=> ({
